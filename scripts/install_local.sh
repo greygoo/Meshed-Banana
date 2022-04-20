74f77a9f2a64b241ba178b78c74727718f56e553
greygoo <greygoo@3d.localdomain>
2022-04-21 00:11:01 +0200

    Add package install script to local installer

--- scripts/install_local.sh
+++ scripts/install_local.sh
@@ -3,6 +3,9 @@
 DEFAULT_USER=$USER
 echo "Installing for user $USER"
 
+# Install required packages
+sudo snippets/install_packages.sh
+
 # Install Soundmodem
 sudo snippets/install_soundmodem.sh
 
---
+++ scripts/snippets/install_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "\nRunning ${0}\n"
+
+apt-get install -y python3-pip python3-dev libffi-dev rustc cargo python3-setuptools soundmodem direwolf docker docker-compose tzdata build-essential libssl-dev python-dev-is-python3 curl mc nano git vim screen python3.9-venv proj-bin libproj-dev imagemagick i2pd js8call cmake libbz2-dev
